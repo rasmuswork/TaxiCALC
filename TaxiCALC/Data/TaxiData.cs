5ce583ffd4de6a331e43f8d08df868885f010f2d
skoleoplaeringscenter <kuraihjorth@gmail.com>
2023-08-10 13:51:35 +0200

    updated code

--- TaxiCALC/Data/TaxiData.cs
+++ TaxiCALC/Data/TaxiData.cs
@@ -1,3 +1,4 @@
+
 namespace TaxiCALC.Data
 {
     public enum vognType
@@ -23,17 +24,36 @@ namespace TaxiCALC.Data
         internal decimal TotalPris;
     }
 
-    public class CustomerChoices
-    {
-        public bool TransportAfCykel { get; set; }
-        public bool Opbaering { get; set; }
-        public bool HjaelpTilOpbaering { get; set; }
-        public bool Lufthavn { get; set; }
-        public bool AfhentningILufthavn { get; set; }
-        public bool FemPersonerEllerOver { get; set; }
-        public bool Liftvogn { get; set; }
-        public bool Oeresundsbro { get; set; }
-        public bool Storebaeltsbroen { get; set; }
-        public decimal DistanceInKm { get; set; }
-    }
+    
+    
+        public class CustomerChoices
+        {
+            public vognType SelectedVognType { get; set; }
+            public bool TransportAfCykel { get; set; }
+            public bool Opbaering { get; set; }
+            public bool HjaelpTilOpbaering { get; set; }
+            public bool Lufthavn { get; set; }
+            public bool AfhentningILufthavn { get; set; }
+            public bool FemPersonerEllerOver { get; set; }
+            public bool Liftvogn { get; set; }
+            public bool Oeresundsbro { get; set; }
+            public bool Storebaeltsbroen { get; set; }
+            public int EstimatedTime { get; set; }
+            public decimal DistanceInKm { get; set; }
+
+            public decimal? TotalPris { get; set; }
+
+            // Additional fee amounts
+            public decimal CykelTillaeg { get; set; } = 30.00m;
+            public decimal OpbaeringTillaeg { get; set; } = 30.00m;
+            public decimal HjaelpTilOpbaeringTillaeg { get; set; } = 30.00m;
+            public decimal LufthavnTillaeg { get; set; } = 15.00m;
+            public decimal FemPersonerEllerOverTillaeg { get; set; } = 40.00m;
+            public decimal LiftvognTillaeg { get; set; } = 350.00m;
+            public decimal OeresundsbroTillaeg { get; set; } = 350.00m;
+            public decimal StorebaeltsbroenTillaeg { get; set; } = 540.00m;
+        }
+    
+
+
 }
